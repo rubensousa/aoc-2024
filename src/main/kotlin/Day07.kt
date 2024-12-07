153b93b16e4c90047fc29a39b86be57df370899b
Ruben Sousa <ruben@rubensousa.com>
2024-12-07 15:51:43 +0100

    Improve solution by using DFS

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -1,4 +1,5 @@
 import kotlin.math.pow
+import kotlin.time.measureTime
 
 object Day07 {
 
@@ -16,151 +17,82 @@ object Day07 {
             )
         }
         // 66343330034722
-        getValidEquationsSum(
-            equations = equations,
-            permutationCalculator = { equation -> getPermutationsPart1(equation) },
-            calculator = { numbers, permutation -> getCalculationInOrder(numbers, permutation) }
-        ).printObject()
+        measureTime {
+            getValidEquationsDfs(
+                equations = equations,
+                supportedOperations = setOf(Operation.ADD, Operation.MULTIPLY)
+            ).printObject()
+        }.inWholeMilliseconds.printObject()
+
         // 637696070419031
-        getValidEquationsSum(
-            equations = equations,
-            permutationCalculator = { equation -> getPermutationsPart2(equation) },
-            calculator = { numbers, permutation -> getCalculationWithConcatenation(numbers, permutation) }
-        ).printObject()
+        measureTime {
+            getValidEquationsDfs(
+                equations = equations,
+                supportedOperations = setOf(Operation.ADD, Operation.MULTIPLY, Operation.CONCATENATION)
+            ).printObject()
+        }.inWholeMilliseconds.printObject()
     }
 
-    private fun getValidEquationsSum(
+    private fun getValidEquationsDfs(
         equations: List<Equation>,
-        permutationCalculator: (Equation) -> List<List<Operation>>,
-        calculator: (List<Long>, List<Operation>) -> Long
+        supportedOperations: Set<Operation>
     ): Long {
         var sum = 0L
         equations.forEach { equation ->
-            val permutations = permutationCalculator(equation)
-            var isValid = false
-            var i = 0
-            while (i < permutations.size) {
-                val result = calculator(equation.fields, permutations[i])
-                if (result == equation.result) {
-                    isValid = true
-                    break
-                }
-                i++
-            }
-            if (isValid) {
+            if (hasValidSolutionIterativeDfs(equation, supportedOperations)) {
                 sum += equation.result
             }
         }
         return sum
     }
 
-    fun getCalculationInOrder(numbers: List<Long>, operations: List<Operation>): Long {
-        var result = 0L
-        var i = 0
-        var operationIndex = 0
-        while (i < numbers.size - 1) {
-            val operation = operations[operationIndex]
-            val currentValue = if (i == 0) {
-                numbers[0]
-            } else {
-                result
-            }
-            result = when (operation) {
-                Operation.ADD -> currentValue + numbers[i + 1]
-                Operation.MULTIPLY -> currentValue * numbers[i + 1]
-                Operation.CONCATENATION -> result
-            }
-            operationIndex++
-            i++
-        }
-        return result
-    }
-
-    fun getCalculationWithConcatenation(numbers: List<Long>, operations: List<Operation>): Long {
-        var result = 0L
-        var i = 0
-        var operationIndex = 0
-        while (i < numbers.size - 1) {
-            val operation = operations[operationIndex]
-            val currentValue = if (i == 0) {
-                numbers[0]
-            } else {
-                result
-            }
-            result = when (operation) {
-                Operation.ADD -> currentValue + numbers[i + 1]
-                Operation.MULTIPLY -> currentValue * numbers[i + 1]
-                Operation.CONCATENATION -> {
-                    val exponent = numbers[i + 1].toString().length
-                    val base = 10.0.pow(exponent.toDouble()).toLong()
-                    currentValue * base + numbers[i + 1]
+    private fun hasValidSolutionIterativeDfs(equation: Equation, supportedOperations: Set<Operation>): Boolean {
+        val stack = mutableListOf<ResultState>()
+        stack.add(ResultState(index = -1, result = 0L))
+        val numbers = equation.fields
+        val expectedResult = equation.result
+        while (stack.isNotEmpty()) {
+            val currentState = stack.removeLast()
+            val index = currentState.index
+            val result = currentState.result
+            if (index == numbers.size - 1) {
+                if (result == expectedResult) {
+                    // Match found, exit
+                    return true
                 }
-
+                continue
             }
-            operationIndex++
-            i++
+            supportedOperations.forEach { operation ->
+                val nextNumber = numbers[index + 1]
+                val nextResult = when (operation) {
+                    Operation.ADD -> result + nextNumber
+                    Operation.MULTIPLY -> result * nextNumber
+                    Operation.CONCATENATION -> {
+                        val exponent = getDigits(nextNumber)
+                        val base = 10.0.pow(exponent.toDouble()).toLong()
+                        result * base + nextNumber
+                    }
+                }
+                if (nextResult <= expectedResult) {
+                    stack.add(ResultState(index = index + 1, nextResult))
+                }
+            }
         }
-        return result
+        return false
     }
 
+    private fun getDigits(number: Long): Long {
+        var currentDigit = 1L
+        var currentNumber = number / 10
+        while (currentNumber > 0) {
+            currentDigit++
+            currentNumber /= 10
+        }
+        return currentDigit
+    }
+
+    data class ResultState(val index: Int, val result: Long)
     data class Equation(val result: Long, val fields: List<Long>)
-
-    private fun getPermutationsPart1(equation: Equation): List<List<Operation>> {
-        val output = mutableListOf<MutableList<Operation>>()
-        Operation.entries.forEach { operation ->
-            val firstList = mutableListOf<Operation>()
-            firstList.add(operation)
-            output.add(firstList)
-        }
-        repeat(equation.fields.size - 2) {
-            permutationsPart1(output)
-        }
-        return output
-    }
-
-    private fun getPermutationsPart2(equation: Equation): List<List<Operation>> {
-        val output = mutableListOf<MutableList<Operation>>()
-        Operation.entries.forEach { operation ->
-            val firstList = mutableListOf<Operation>()
-            firstList.add(operation)
-            output.add(firstList)
-        }
-        repeat(equation.fields.size - 2) {
-            permutationsPart2(output)
-        }
-        return output
-    }
-
-    private fun permutationsPart1(output: MutableList<MutableList<Operation>>) {
-        val newLists = mutableListOf<MutableList<Operation>>()
-        output.forEach { list ->
-            val newList = mutableListOf<Operation>()
-            newList.addAll(list)
-            newList.add(Operation.ADD)
-            newLists.add(newList)
-            val newList2 = mutableListOf<Operation>()
-            newList2.addAll(list)
-            newList2.add(Operation.MULTIPLY)
-            newLists.add(newList2)
-        }
-        output.clear()
-        output.addAll(newLists)
-    }
-
-    private fun permutationsPart2(output: MutableList<MutableList<Operation>>) {
-        val newLists = mutableListOf<MutableList<Operation>>()
-        output.forEach { list ->
-            Operation.entries.forEach { operation ->
-                val newList = mutableListOf<Operation>()
-                newList.addAll(list)
-                newList.add(operation)
-                newLists.add(newList)
-            }
-        }
-        output.clear()
-        output.addAll(newLists)
-    }
-
     enum class Operation {
         ADD,
         MULTIPLY,
