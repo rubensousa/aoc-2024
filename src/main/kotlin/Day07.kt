dd67e89e7141367538d650b67570444bbfcfd35b
Ruben Sousa <ruben@rubensousa.com>
2024-12-07 16:03:11 +0100

    Use recursive DFS to avoid object allocation

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -18,7 +18,7 @@ object Day07 {
         }
         // 66343330034722
         measureTime {
-            getValidEquationsDfs(
+            getValidEquationsRecursiveDfs(
                 equations = equations,
                 supportedOperations = setOf(Operation.ADD, Operation.MULTIPLY)
             ).printObject()
@@ -26,59 +26,61 @@ object Day07 {
 
         // 637696070419031
         measureTime {
-            getValidEquationsDfs(
+            getValidEquationsRecursiveDfs(
                 equations = equations,
                 supportedOperations = setOf(Operation.ADD, Operation.MULTIPLY, Operation.CONCATENATION)
             ).printObject()
         }.inWholeMilliseconds.printObject()
     }
 
-    private fun getValidEquationsDfs(
+    private fun getValidEquationsRecursiveDfs(
         equations: List<Equation>,
         supportedOperations: Set<Operation>
     ): Long {
         var sum = 0L
         equations.forEach { equation ->
-            if (hasValidSolutionIterativeDfs(equation, supportedOperations)) {
+            if (dfs(equation, supportedOperations, currentIndex = -1, result = 0L)) {
                 sum += equation.result
             }
         }
         return sum
     }
 
-    private fun hasValidSolutionIterativeDfs(equation: Equation, supportedOperations: Set<Operation>): Boolean {
-        val stack = mutableListOf<ResultState>()
-        stack.add(ResultState(index = -1, result = 0L))
-        val numbers = equation.fields
-        val expectedResult = equation.result
-        while (stack.isNotEmpty()) {
-            val currentState = stack.removeLast()
-            val index = currentState.index
-            val result = currentState.result
-            if (index == numbers.size - 1) {
-                if (result == expectedResult) {
-                    // Match found, exit
+    private fun dfs(
+        equation: Equation,
+        supportedOperations: Set<Operation>,
+        currentIndex: Int,
+        result: Long
+    ): Boolean {
+        if (currentIndex == equation.fields.size - 1) {
+            return result == equation.result
+        }
+        val nextNumber = equation.fields[currentIndex + 1]
+        supportedOperations.forEach { operation ->
+            val nextResult = calculateNextResult(result, nextNumber, operation)
+            if (nextResult <= equation.result) {
+                if (dfs(equation, supportedOperations, currentIndex + 1, nextResult)) {
                     return true
-                }
-                continue
-            }
-            supportedOperations.forEach { operation ->
-                val nextNumber = numbers[index + 1]
-                val nextResult = when (operation) {
-                    Operation.ADD -> result + nextNumber
-                    Operation.MULTIPLY -> result * nextNumber
-                    Operation.CONCATENATION -> {
-                        val exponent = getDigits(nextNumber)
-                        val base = 10.0.pow(exponent.toDouble()).toLong()
-                        result * base + nextNumber
-                    }
-                }
-                if (nextResult <= expectedResult) {
-                    stack.add(ResultState(index = index + 1, nextResult))
                 }
             }
         }
         return false
+    }
+
+    private fun calculateNextResult(
+        currentResult: Long,
+        nextNumber: Long,
+        operation: Operation
+    ): Long {
+        return when (operation) {
+            Operation.ADD -> currentResult + nextNumber
+            Operation.MULTIPLY -> currentResult * nextNumber
+            Operation.CONCATENATION -> {
+                val exponent = getDigits(nextNumber)
+                val base = 10.0.pow(exponent.toDouble()).toLong()
+                currentResult * base + nextNumber
+            }
+        }
     }
 
     private fun getDigits(number: Long): Long {
@@ -91,7 +93,6 @@ object Day07 {
         return currentDigit
     }
 
-    data class ResultState(val index: Int, val result: Long)
     data class Equation(val result: Long, val fields: List<Long>)
     enum class Operation {
         ADD,
