09524a00f0f5271898c6a4eb99899fb1c68b2944
Ruben Sousa <ruben@rubensousa.com>
2024-12-08 14:31:36 +0100

    Delete inputs

--- src/main/kotlin/FileExtensions.kt
+++ src/main/kotlin/FileExtensions.kt
@@ -1,9 +1,9 @@
 import java.io.File
 
 fun readText(path: String): List<String> {
-    return File(path).readLines()
+    return File("inputs/$path").readLines()
 }
 
 fun readLine(path: String): String {
-    return File(path).readText()
+    return File("inputs/$path").readText()
 }
