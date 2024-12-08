01f0172891494bb95006b1eb0369e56808814f5e
Ruben Sousa <ruben@rubensousa.com>
2024-12-08 11:01:59 +0100

    Add solution for day 8

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -5,12 +5,16 @@ object Day08 {
     @JvmStatic
     fun main(args: Array<String>) {
         val testInput = readText("day08.txt")
-        val grid = Grid()
+        val firstGrid = Grid()
+        val secondGrid = Grid()
         testInput.forEach { line ->
-            grid.fillRow(line)
+            firstGrid.fillRow(line)
+            secondGrid.fillRow(line)
         }
         // 259
-        findAntinodes(grid).printObject()
+        findAntinodes(firstGrid).printObject()
+        // 927
+        findResonantAntinodes(secondGrid).printObject()
     }
 
     fun findAntinodes(grid: Grid): Int {
@@ -19,52 +23,119 @@ object Day08 {
         for (row in 0 until totalRows) {
             for (col in 0 until totalColumns) {
                 grid.findAntenna(row, col)?.let { char ->
-                    fillAntinodes(row, col, char, grid)
+                    for (j in col + 1 until totalColumns) {
+                        if (grid.hasAntenna(row, j, char)) {
+                            placeAntinodes(row, col, row, j, grid, false) { _, _, _, _ -> false }
+                        }
+                    }
+                    for (i in row + 1 until totalRows) {
+                        for (j in 0 until totalColumns) {
+                            if (grid.hasAntenna(i, j, char)) {
+                                placeAntinodes(row, col, i, j, grid, false) { _, _, _, _ ->
+                                    false
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
         return grid.getAntinodes()
     }
 
-    private fun fillAntinodes(row: Int, col: Int, char: Char, grid: Grid) {
+    fun findResonantAntinodes(grid: Grid): Int {
         val totalRows = grid.getRows()
         val totalColumns = grid.getColumns()
-
-        // Search same row
-        for (j in col + 1 until totalColumns) {
-            if (grid.hasAntenna(row, j, char)) {
-                val distance = abs(j - col)
-                // Place backwards
-                grid.placeAntinode(row, col - distance)
-                // Place forwards
-                grid.placeAntinode(row, j + distance)
-            }
-        }
-
-        // Search downwards
-        for (i in row + 1 until totalRows) {
-            for (j in 0 until totalColumns) {
-                if (grid.hasAntenna(i, j, char)) {
-                    val verticalDistance = i - row
-                    val horizontalDistance = abs(j - col)
-                    if (j == col) {
-                        grid.placeAntinode(row - verticalDistance, col)
-                        grid.placeAntinode(i + verticalDistance, col)
-                    } else if (j > col) {
-                        grid.placeAntinode(row - verticalDistance, col - horizontalDistance)
-                        grid.placeAntinode(i + verticalDistance, j + horizontalDistance)
-                    } else {
-                        grid.placeAntinode(row - verticalDistance, col + horizontalDistance)
-                        grid.placeAntinode(i + verticalDistance, j - horizontalDistance)
+        for (row in 0 until totalRows) {
+            for (col in 0 until totalColumns) {
+                grid.findAntenna(row, col)?.let { char ->
+                    for (j in col + 1 until totalColumns) {
+                        if (grid.hasAntenna(row, j, char)) {
+                            placeAntinodes(row, col, row, j, grid, includeAntennas = true) { _, _, startCol, endCol ->
+                                startCol >= 0 || endCol <= totalColumns - 1
+                            }
+                        }
+                    }
+                    for (i in row + 1 until totalRows) {
+                        for (j in 0 until totalColumns) {
+                            if (grid.hasAntenna(i, j, char)) {
+                                placeAntinodes(row, col, i, j, grid, includeAntennas = true) { startRow, endRow, _, _ ->
+                                    startRow >= 0 || endRow <= totalRows - 1
+                                }
+                            }
+                        }
                     }
                 }
             }
+        }
+        return grid.getAntinodes()
+    }
+
+    private fun placeAntinodes(
+        originRow: Int,
+        originCol: Int,
+        targetRow: Int,
+        targetCol: Int,
+        grid: Grid,
+        includeAntennas: Boolean,
+        continuePlacement: (startRow: Int, endRow: Int, startCol: Int, endCol: Int) -> Boolean,
+    ) {
+        val verticalDistance = targetRow - originRow
+        val horizontalDistance = abs(targetCol - originCol)
+        var startCol = if (targetCol > originCol) {
+            originCol - horizontalDistance
+        } else {
+            originCol + horizontalDistance
+        }
+        var endCol = if (targetCol > originCol) {
+            targetCol + horizontalDistance
+        } else {
+            targetCol - horizontalDistance
+        }
+        var startRow = originRow - verticalDistance
+        var endRow = targetRow + verticalDistance
+        do {
+            grid.placeAntinode(startRow, startCol)
+            grid.placeAntinode(endRow, endCol)
+            if (targetCol > originCol) {
+                startCol -= horizontalDistance
+                endCol += horizontalDistance
+            } else {
+                startCol += horizontalDistance
+                endCol -= horizontalDistance
+            }
+            startRow -= verticalDistance
+            endRow += verticalDistance
+        } while (continuePlacement(startRow, endRow, startCol, endCol))
+
+        if (includeAntennas) {
+            grid.placeAntinode(originRow, originCol)
+            grid.placeAntinode(targetRow, targetCol)
         }
     }
 
     class Grid {
 
         private val cells = mutableMapOf<Int, MutableMap<Int, MutableSet<Cell>>>()
+        private var antinodes = 0
+
+        fun print() {
+            cells.values.forEach { row ->
+                row.values.forEach { set ->
+                    if (set.isEmpty()) {
+                        print(".")
+                    } else {
+                        val antenna = set.find { it is Cell.Antenna }
+                        if (antenna != null) {
+                            print(antenna.toString())
+                        } else {
+                            print(Cell.Antinode.toString())
+                        }
+                    }
+                }
+                println()
+            }
+        }
 
         fun getRows() = cells.size
 
@@ -96,27 +167,32 @@ object Day08 {
             return findAntenna(row, col) == char
         }
 
-        fun getAntinodes(): Int {
-            var count = 0
-            cells.values.forEach { entry ->
-                entry.values.forEach { element ->
-                    if (element.contains(Cell.Antinode)) {
-                        count++
-                    }
-                }
-            }
-            return count
+        fun hasAntinode(row: Int, col: Int): Boolean {
+            val set = cells[row]?.get(col) ?: emptySet()
+            return set.contains(Cell.Antinode)
         }
 
+        fun getAntinodes(): Int = antinodes
+
         fun placeAntinode(row: Int, col: Int) {
-            cells[row]?.get(col)?.add(Cell.Antinode)
+            cells[row]?.get(col)?.let { set ->
+                if (!set.contains(Cell.Antinode)) {
+                    antinodes++
+                    set.add(Cell.Antinode)
+                }
+            }
         }
 
     }
 
     sealed interface Cell {
-        data class Antenna(val char: Char) : Cell
-        data object Antinode : Cell
+        data class Antenna(val char: Char) : Cell {
+            override fun toString(): String = char.toString()
+        }
+
+        data object Antinode : Cell {
+            override fun toString(): String = "#"
+        }
     }
 
 }
--- src/test/kotlin/Day08Test.kt
+++ src/test/kotlin/Day08Test.kt
@@ -11,6 +11,75 @@ class Day08Test {
     }
 
     @Test
+    fun `test searching within same row with resonant`() {
+        val grid = Day08.Grid()
+        grid.fillRow("A..A.............")
+        val nodes = Day08.findResonantAntinodes(grid)
+        grid.print()
+        assertThat(nodes).isEqualTo(6)
+    }
+
+    @Test
+    fun `searching within positive diagonal with resonancy`() {
+        val lines = listOf(
+            "A........",
+            ".A.......",
+            ".........",
+            ".........",
+            ".........",
+        )
+        val grid = Day08.Grid()
+        lines.forEach { line ->
+            grid.fillRow(line)
+        }
+        val nodes = Day08.findResonantAntinodes(grid)
+        grid.print()
+        assertThat(nodes).isEqualTo(5)
+        repeat(5) { index ->
+            assertThat(grid.hasAntinode(index, index)).isTrue()
+        }
+    }
+
+    @Test
+    fun `searching within negative diagonal with resonancy`() {
+        val lines = listOf(
+            "........A",
+            ".......A.",
+            ".........",
+            ".........",
+            ".........",
+        )
+        val grid = Day08.Grid()
+        lines.forEach { line ->
+            grid.fillRow(line)
+        }
+        val nodes = Day08.findResonantAntinodes(grid)
+        grid.print()
+        assertThat(nodes).isEqualTo(5)
+        repeat(5) { index ->
+            assertThat(grid.hasAntinode(index, grid.getColumns() - 1 - index)).isTrue()
+        }
+    }
+
+    @Test
+    fun `searching within multiple shared cells with resonancy`() {
+        val lines = listOf(
+            "A.....O...",
+            "...A..O...",
+            "..........",
+            "..........",
+        )
+        val grid = Day08.Grid()
+        lines.forEach { line ->
+            grid.fillRow(line)
+        }
+        val nodes = Day08.findResonantAntinodes(grid)
+        grid.print()
+        assertThat(nodes).isEqualTo(7)
+    }
+
+
+    @Test
     fun `test example grid`() {
         val lines = readText("day08_example.txt")
         val grid = Day08.Grid()
@@ -21,4 +90,15 @@ class Day08Test {
         assertThat(nodes).isEqualTo(14)
     }
 
+    @Test
+    fun `test example grid resonancy`() {
+        val lines = readText("day08_example.txt")
+        val grid = Day08.Grid()
+        lines.forEach { line ->
+            grid.fillRow(line)
+        }
+        val nodes = Day08.findResonantAntinodes(grid)
+        grid.print()
+        assertThat(nodes).isEqualTo(34)
+    }
 }
