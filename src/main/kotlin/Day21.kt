c54fa091b5c3dc6b894081c45ac2e65f9be0aae4
Rúben Sousa <ruben@rubensousa.com>
2024-12-23 16:52:29 +0000

    Add solution for day 21

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -1,13 +1,16 @@
-import kotlin.math.abs
+import grid.Point
 
 object Day21 {
+
+    private val dpadPaths = buildShortestPath(buttons = Dpad.entries.toList())
+    private val keypadPaths = buildShortestPath(buttons = Keypad.entries.toList())
 
     @JvmStatic
     fun main(args: Array<String>) {
         val lines = readText("day21.txt")
         val sequences = mutableListOf<Sequence>()
         lines.forEach { line ->
-            sequences.add(Sequence(digits = line.toCharArray().toList()))
+            sequences.add(Sequence(line))
         }
 
         // 1429
@@ -20,212 +23,125 @@ object Day21 {
         }
     }
 
-    data class Sequence(val digits: List<Char>)
+    data class Sequence(val value: String)
 
     private fun part1(sequences: List<Sequence>): Long {
-        var sum = 0L
-        sequences.forEach { sequence ->
-            val buttonSequence = findButtonSequence(sequence)
-            sum += buttonSequence.length * getNumericPart(sequence)
+        return sequences.sumOf { sequence ->
+            val length = calculateSequenceLength(sequence, layers = 3)
+            length * getNumericPart(sequence)
         }
-        return sum
+    }
+
+    private fun part2(sequences: List<Sequence>): Long {
+        return sequences.sumOf { sequence ->
+            val length = calculateSequenceLength(sequence, layers = 26)
+            length * getNumericPart(sequence)
+        }
     }
 
     private fun getNumericPart(sequence: Sequence): Int {
-        val intPart = String(sequence.digits.toCharArray()).substring(0, sequence.digits.size - 1)
+        val intPart = sequence.value.substring(0, sequence.value.length - 1)
         return intPart.toInt()
     }
 
-    private fun findButtonSequence(sequence: Sequence): String {
-        val dpadStates = MutableList(3) { Dpad.CLICK }
-        var currentKey = Key.CLICK
-        var currentDpad = Dpad.CLICK
-        val keyPadRobotSequence = StringBuilder()
-        sequence.digits.forEach { digit ->
-            val targetKey = getKey(digit)
-            val keySequence = getLastSequenceForKey(
-                currentKey = currentKey,
-                target = targetKey
-            )
-            currentKey = targetKey
-            if (keySequence.isNotEmpty()) {
-                keyPadRobotSequence.append(keySequence)
-            }
-            keyPadRobotSequence.append(Key.CLICK.char)
-        }
-        currentKey = Key.CLICK
-        currentDpad = Dpad.CLICK
-        val secondRobotSequence = StringBuilder()
-        keyPadRobotSequence.forEach { digit ->
-            val targetDpad = getDpad(digit)
-            val keySequence = getLastSequenceForDpad(
-                currentDpad = currentDpad,
-                target = targetDpad
-            )
-            currentDpad = targetDpad
-            if (keySequence.isNotEmpty()) {
-                secondRobotSequence.append(keySequence)
-            }
-            secondRobotSequence.append(Dpad.CLICK.char)
-        }
-        currentDpad = Dpad.CLICK
-        val thirdRobotSequence = StringBuilder()
-        secondRobotSequence.forEach { digit ->
-            val targetDpad = getDpad(digit)
-            val keySequence = getLastSequenceForDpad(
-                currentDpad = currentDpad,
-                target = targetDpad
-            )
-            currentDpad = targetDpad
-            if (keySequence.isNotEmpty()) {
-                thirdRobotSequence.append(keySequence)
-            }
-            thirdRobotSequence.append(Dpad.CLICK.char)
-        }
-        return thirdRobotSequence.toString()
-    }
-
-    private fun getLastSequenceForKey(currentKey: Key, target: Key): String {
-        return getKeypadSequence(
-            startX = currentKey.x,
-            startY = currentKey.y,
-            targetX = target.x,
-            targetY = target.y
+    private fun calculateSequenceLength(sequence: Sequence, layers: Int): Long {
+        return findSequenceLength(
+            sequence = sequence.value,
+            layer = layers,
+            shortestPaths = keypadPaths,
+            calculatedLengths = mutableMapOf()
         )
     }
 
-    private fun getLastSequenceForDpad(currentDpad: Dpad, target: Dpad): String {
-        return getDpadSequence(
-            startX = currentDpad.x,
-            startY = currentDpad.y,
-            targetX = target.x,
-            targetY = target.y
-        )
-    }
-
-    private fun getKeypadSequence(startX: Int, startY: Int, targetX: Int, targetY: Int): String {
-        val xDiff = targetX - startX
-        val yDiff = targetY - startY
-        val stringBuilder = StringBuilder()
-        if (startY == targetY) {
-            repeat(abs(xDiff)) {
-                if (xDiff > 0) {
-                    stringBuilder.append(Dpad.RIGHT.char)
-                } else {
-                    stringBuilder.append(Dpad.LEFT.char)
-                }
-            }
-        } else if (startX == targetX) {
-            repeat(abs(yDiff)) {
-                if (yDiff > 0) {
-                    stringBuilder.append(Dpad.DOWN.char)
-                } else {
-                    stringBuilder.append(Dpad.UP.char)
-                }
-            }
-        } else {
-            if (startX == Key.SEVEN.x) {
-                repeat(xDiff) {
-                    stringBuilder.append(Dpad.RIGHT.char)
-                }
-                repeat(abs(yDiff)) {
-                    if (yDiff > 0) {
-                        stringBuilder.append(Dpad.DOWN.char)
-                    } else {
-                        stringBuilder.append(Dpad.UP.char)
-                    }
-                }
+    private fun findSequenceLength(
+        sequence: String,
+        layer: Int,
+        shortestPaths: Map<Char, Map<Char, List<String>>>,
+        calculatedLengths: MutableMap<Pair<String, Int>, Long>
+    ): Long {
+        return calculatedLengths.getOrPut(sequence to layer) {
+            if (layer == 0) {
+                sequence.length.toLong()
             } else {
-                repeat(abs(yDiff)) {
-                    if (yDiff > 0) {
-                        stringBuilder.append(Dpad.DOWN.char)
-                    } else {
-                        stringBuilder.append(Dpad.UP.char)
-                    }
-                }
-                repeat(abs(xDiff)) {
-                    if (xDiff > 0) {
-                        stringBuilder.append(Dpad.RIGHT.char)
-                    } else {
-                        stringBuilder.append(Dpad.LEFT.char)
+                val newSequence = "A$sequence"
+                // Find the shortest path from every consecutive char and add them up
+                newSequence.zipWithNext().sumOf { (from, to) ->
+                    shortestPaths[from]!![to]!!.minOf { path ->
+                        findSequenceLength(
+                            sequence = "${path}A",
+                            layer = layer - 1,
+                            shortestPaths = dpadPaths,
+                            calculatedLengths = calculatedLengths
+                        )
                     }
                 }
             }
         }
-        return stringBuilder.toString()
     }
 
-    private fun getDpadSequence(startX: Int, startY: Int, targetX: Int, targetY: Int): String {
-        val xDiff = targetX - startX
-        val yDiff = targetY - startY
-        val stringBuilder = StringBuilder()
-        if (startY == targetY) {
-            repeat(abs(xDiff)) {
-                if (xDiff > 0) {
-                    stringBuilder.append(Dpad.RIGHT.char)
-                } else {
-                    stringBuilder.append(Dpad.LEFT.char)
-                }
-            }
-        } else if (startX == targetX) {
-            repeat(abs(yDiff)) {
-                if (yDiff > 0) {
-                    stringBuilder.append(Dpad.DOWN.char)
-                } else {
-                    stringBuilder.append(Dpad.UP.char)
-                }
-            }
-        } else {
-            if (startX == Dpad.LEFT.x) {
-                repeat(xDiff) {
-                    stringBuilder.append(Dpad.RIGHT.char)
-                }
-                stringBuilder.append(Dpad.UP.char)
-            } else {
-                repeat(abs(yDiff)) {
-                    if (yDiff > 0) {
-                        stringBuilder.append(Dpad.DOWN.char)
-                    } else {
-                        stringBuilder.append(Dpad.UP.char)
-                    }
-                }
-                repeat(abs(xDiff)) {
-                    if (xDiff > 0) {
-                        stringBuilder.append(Dpad.RIGHT.char)
-                    } else {
-                        stringBuilder.append(Dpad.LEFT.char)
+    private fun buildShortestPath(buttons: List<Button>): Map<Char, Map<Char, List<String>>> {
+        val output = mutableMapOf<Char, MutableMap<Char, MutableList<String>>>()
+        val validPoints = buttons.associateBy { it.point }
+        buttons.forEach { button ->
+            val paths = mutableMapOf<Char, MutableList<String>>()
+            paths[button.char] = mutableListOf("")
+            val queue = ArrayDeque<Pair<Button, String>>()
+            queue.addLast(button to "")
+            val visited = mutableSetOf<Point>()
+            while (queue.isNotEmpty()) {
+                val currentState = queue.removeFirst()
+                val currentButton = currentState.first
+                val currentPath = currentState.second
+                visited.add(currentButton.point)
+                currentButton.point.neighbours().forEach { point ->
+                    if (validPoints.containsKey(point) && !visited.contains(point)) {
+                        val nextButton = validPoints[point]!!
+                        val diffPoint = nextButton.point - currentButton.point
+                        val newPath = currentPath + getDirection(diffPoint).char
+                        queue.addLast(Pair(first = nextButton, second = newPath))
+                        paths.getOrPut(nextButton.char) { mutableListOf() } += newPath
                     }
                 }
             }
+            output[button.char] = paths
         }
-        return stringBuilder.toString()
+        return output
     }
 
-    private fun getKey(char: Char): Key {
-        return Key.entries.find { it.char == char }!!
+    private fun getDirection(point: Point): Dpad {
+        if (point.x == -1) {
+            return Dpad.LEFT
+        }
+        if (point.x == 1) {
+            return Dpad.RIGHT
+        }
+        if (point.y == 1) {
+            return Dpad.DOWN
+        }
+        if (point.y == -1) {
+            return Dpad.UP
+        }
+        throw IllegalArgumentException("Directional point is invalid")
     }
 
-    private fun getDpad(char: Char): Dpad {
-        return Dpad.entries.find { it.char == char }!!
+    interface Button {
+        val point: Point
+        val char: Char
     }
 
-    private fun part2(sequences: List<Sequence>): Int {
-        return 0
+    enum class Dpad(override val point: Point, override val char: Char) : Button {
+        UP(Point(1, 0), '^'),
+        CLICK(Point(2, 0), 'A'),
+        LEFT(Point(0, 1), '<'),
+        DOWN(Point(1, 1), 'v'),
+        RIGHT(Point(2, 1), '>')
     }
 
-    enum class Dpad(val x: Int, val y: Int, val char: Char) {
-        UP(1, 0, '^'),
-        CLICK(2, 0, 'A'),
-        LEFT(0, 1, '<'),
-        DOWN(1, 1, 'v'),
-        RIGHT(2, 1, '>')
-    }
-
-    enum class Key(val x: Int, val y: Int, val char: Char) {
-        SEVEN(0, 0, '7'), EIGHT(1, 0, '8'), NINE(2, 0, '9'),
-        FOUR(0, 1, '4'), FIVE(1, 1, '5'), SIX(2, 1, '6'),
-        ONE(0, 2, '1'), TWO(1, 2, '2'), THREE(2, 2, '3'),
-        ZERO(1, 3, '0'), CLICK(2, 3, 'A'),
+    enum class Keypad(override val point: Point, override val char: Char) : Button {
+        SEVEN(Point(0, 0), '7'), EIGHT(Point(1, 0), '8'), NINE(Point(2, 0), '9'),
+        FOUR(Point(0, 1), '4'), FIVE(Point(1, 1), '5'), SIX(Point(2, 1), '6'),
+        ONE(Point(0, 2), '1'), TWO(Point(1, 2), '2'), THREE(Point(2, 2), '3'),
+        ZERO(Point(1, 3), '0'), CLICK(Point(2, 3), 'A'),
     }
 
 }
--- src/main/kotlin/grid/Point.kt
+++ src/main/kotlin/grid/Point.kt
@@ -12,4 +12,14 @@ data class Point(val x: Int, val y: Int) {
         return Point(x = x + direction.col * times, y = y + direction.row * times)
     }
 
+    operator fun plus(other: Point) = Point(x + other.x, y + other.y)
+
+    operator fun minus(other: Point) = Point(x - other.x, y - other.y)
+
+    fun neighbours(): List<Point> {
+        return Direction.entries.map { direction ->
+            this.move(direction)
+        }
+    }
+
 }
